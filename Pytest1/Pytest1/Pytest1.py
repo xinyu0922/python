d2549ec9c0b937159a054a8597ded283ea82c802
Vincent <Vincent@DESKTOP-E48OAV7>
2019-01-09 00:41:05 +0800

    首次提交

---
+++ Pytest1/Pytest1/Pytest1.py
@@ -0,0 +1,26 @@
+print ("hello word")
+#打印
+
+
+a="hello word"
+print(a)
+#变量打印
+
+
+name="ada lovelace"
+print(name.title())
+#首字母大写
+name="ada lovelace"
+print(name.upper()) #全部大写
+print(name.lower()) #全部小写
+
+
+first_name="ada"
+last_name="lovelace"
+full_name=first_name+" "+last_name
+print(full_name)
+print("hello,"+full_name.title()+"!")
+#拼接
+
+
+
