1651f82809901a8a433af37cbbde0165fe0d6eec
Vincent <Vincent@DESKTOP-E48OAV7>
2019-01-11 00:06:33 +0800

    变量跟数据类型

---
+++ Pytest1/Pytest1/变量和数据类型.py
@@ -0,0 +1,58 @@
+'''
+print ("hello word")
+
+
+#变量打印
+a="hello word"
+print(a)
+
+
+#首字母大写
+name="ada lovelace"
+print(name.title())
+
+name="Ada Lovelace"
+print(name.upper()) #全部大写
+print(name.lower()) #全部小写
+
+#拼接
+first_name="ada"
+last_name="lovelace"
+full_name=first_name+" "+last_name
+print(full_name)
+print("hello,"+full_name.title()+"!")
+message="hello,"+full_name+"!"
+print(message)
+
+
+#制表符
+print("print")
+print("\tprint") # + 空格
+print("\nprint") #换行
+print("languages:\n\tjava\n\tpython\n\tC")
+
+
+lanuage="python "
+print(lanuage)
+print(lanuage.rstrip()) #去除末尾空格
+lanuage=lanuage.rstrip()  #永久去除末尾空格
+print(lanuage)
+
+
+lanuage2=" pyhon "
+print(lanuage2.rstrip()) #去除末尾空格
+print(lanuage2.lstrip()) #去除开头空格
+print(lanuage2.strip())  #去除2端空格
+'''
+
+print(2+3)
+print(3-2)
+print(2*3)
+print(4/2)
+
+
+
+
+
+
+
